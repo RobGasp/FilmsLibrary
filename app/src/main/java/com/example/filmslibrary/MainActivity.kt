f2d32c4097771872db7914d42af0c8a64378265a
Michael Makulov <mmn95@mail.ru>
2022-04-21 22:45:15 +0500

    -Добавлен BottomNavigationView на главный экран и его обработка в коде, иконки для меню -Добавлено 3 фрагмента: ActualFragment для отображения актуальных фильмов, FavouriteFragment - для избранного, CinemasFragment - для карты кинотеатров

--- app/src/main/java/com/example/filmslibrary/MainActivity.kt
+++ app/src/main/java/com/example/filmslibrary/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.filmslibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_view_actual -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ActualFragment())
+                        .commitAllowingStateLoss()
+                    true
+                }
+                R.id.bottom_view_favourite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FavouriteFragment())
+                        .commitAllowingStateLoss()
+                    true
+                }
+                R.id.bottom_view_cinemas -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, CinemasFragment())
+                        .commitAllowingStateLoss()
+                    true
+                }
+                else -> false
+            }
+        }
+        bottomNavigationView.selectedItemId = R.id.bottom_view_actual
     }
 }
