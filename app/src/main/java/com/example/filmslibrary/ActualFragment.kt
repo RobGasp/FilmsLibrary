7040f9a027e21944b01c6e472c1bdf306a3ba7f1
Michael Makulov <mmn95@mail.ru>
2022-04-24 22:29:04 +0500

    -В экран актуального добавлен AppBar с поиском

--- app/src/main/java/com/example/filmslibrary/ActualFragment.kt
+++ app/src/main/java/com/example/filmslibrary/ActualFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
 
 class ActualFragment : Fragment() {
 
@@ -15,4 +17,18 @@ class ActualFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_actual, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val topAppBar = view.findViewById<MaterialToolbar>(R.id.topAppBar)
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    Toast.makeText(context, "Нажата кнопка поиска", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 }
