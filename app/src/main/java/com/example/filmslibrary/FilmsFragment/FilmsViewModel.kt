dc55355c939ec0494e3aa3b2e9cf2ed74acccf81
robert <silent.jaguar@gmail.com>
2022-04-25 23:23:54 +0300

    Request to Internet

--- app/src/main/java/com/example/filmslibrary/FilmsFragment/FilmsViewModel.kt
+++ app/src/main/java/com/example/filmslibrary/FilmsFragment/FilmsViewModel.kt
@@ -1,17 +1,12 @@
 package com.example.filmslibrary.FilmsFragment
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.example.filmslibrary.model.AppState
 import com.example.filmslibrary.model.dataSource.DataSource
 import com.example.filmslibrary.model.repository.FilmObject
 import com.example.filmslibrary.model.repository.FilmsRepositoryInterface
 import kotlinx.coroutines.Dispatchers
-import java.lang.Thread.sleep
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import java.lang.Thread.sleep
 
 class FilmsViewModel(private val repositoryInterface: FilmsRepositoryInterface<DataSource<List<FilmObject>>>) :
     ViewModel(), LifecycleObserver {
@@ -20,14 +15,17 @@ class FilmsViewModel(private val repositoryInterface: FilmsRepositoryInterface<D
 
     fun getMyLiveData() = myLiveData
 
-    fun getFilms() {
-        myLiveData.value = AppState.Loading
-
-
+    suspend fun getFilms(apiKey:String, language:String) {
+        myLiveData.value = AppState.Loading(null)
+  
         viewModelScope.async(Dispatchers.IO) {
-            sleep(1000)
-            myLiveData.postValue(AppState.Success(repositoryInterface.getListOfFilmsFromInternetAsync(apiKey:String, )))
-        }
+            myLiveData.postValue(AppState.Success(repositoryInterface.getListOfFilmsFromInternetAsync(apiKey,language)))
+        }.await()
+    }
+
+    override fun onCleared() {
+        myLiveData.value = AppState.Success(null)
+        super.onCleared()
     }
 }
 
--- app/src/main/java/com/example/filmslibrary/model/AppState.kt
+++ app/src/main/java/com/example/filmslibrary/model/AppState.kt
@@ -1,9 +1,10 @@
 package com.example.filmslibrary.model
 
 import com.example.filmslibrary.model.repository.FilmObject
+import kotlinx.coroutines.Deferred
 
 sealed class AppState {
-    data class Success(val filmsData: List<FilmObject>?) : AppState()
+    data class Success(val filmsData: Deferred<List<FilmObject>>?) : AppState()
     data class Error(val error: Throwable) : AppState()
-    object Loading : AppState()
+    data class Loading (val progress:Int?): AppState()
 }
