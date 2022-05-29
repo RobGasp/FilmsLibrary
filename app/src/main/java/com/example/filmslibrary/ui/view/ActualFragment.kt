c6c62324c216b8958b93adcda07e3d01314cfe5b
Michael Makulov <mmn95@mail.ru>
2022-05-29 21:39:40 +0500

    -Небольшие изменения экрана актуального (2 колонки списка вместо 3-х)

--- app/src/main/java/com/example/filmslibrary/ui/view/ActualFragment.kt
+++ app/src/main/java/com/example/filmslibrary/ui/view/ActualFragment.kt
@@ -1,13 +1,10 @@
 package com.example.filmslibrary.ui.view
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,9 +13,7 @@ import com.example.filmslibrary.model.data.AppState
 import com.example.filmslibrary.model.repository.FilmObject
 import com.example.filmslibrary.ui.recyclerViewAdapters.ActualFilmsAdapter
 import com.example.filmslibrary.ui.viewModel.FilmsViewModel
-import kotlinx.coroutines.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.net.URL
 
 class ActualFragment : Fragment() {
 
@@ -74,12 +69,12 @@ class ActualFragment : Fragment() {
     }
 
     private fun initRecyclerView() {
-        val lm = GridLayoutManager(context, 3)
+        val lm = GridLayoutManager(context, 2)
         recyclerView?.layoutManager = lm
         adapter = ActualFilmsAdapter()
         recyclerView?.adapter = adapter
-        adapter?.filmClickListener = ActualFilmsAdapter.FilmClickListener {
-            movie -> filmClicked(movie)
+        adapter?.filmClickListener = ActualFilmsAdapter.FilmClickListener { movie ->
+            filmClicked(movie)
         }
     }
 
@@ -99,7 +94,7 @@ class ActualFragment : Fragment() {
     }
 
 
-     fun filmClicked(film: FilmObject) {
+    fun filmClicked(film: FilmObject) {
         val action =
             ActualFragmentDirections.actionActualFragmentToDetailsPageFragment(movie = film)
         view?.findNavController()?.navigate(action)
