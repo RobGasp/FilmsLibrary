de922ade7a67c4a33f07ffe35c272ea21d73a3a9
Michael Makulov <mmn95@mail.ru>
2022-06-01 21:48:20 +0500

    -Добавлена карта во фрагмент с кинотеатрами

--- app/src/main/java/com/example/filmslibrary/ui/view/CinemasFragment.kt
+++ app/src/main/java/com/example/filmslibrary/ui/view/CinemasFragment.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.filmslibrary.R
 import com.example.filmslibrary.databinding.FragmentCinemasBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class CinemasFragment : Fragment() {
 
     private var _binding: FragmentCinemasBinding? = null
     private val binding get() = _binding!!
+    private val callback = OnMapReadyCallback { googleMap ->
+        val moscow = LatLng(55.45, 37.37)
+        googleMap.addMarker(MarkerOptions().position(moscow).title("Moscow"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(moscow))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +30,13 @@ class CinemasFragment : Fragment() {
     ): View? {
         _binding = FragmentCinemasBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.cinemas_map_fragment) as
+                SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/filmslibrary/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/filmslibrary/ui/view/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.filmslibrary.ui.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,14 +10,12 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.filmslibrary.R
 import com.example.filmslibrary.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,11 +31,15 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.actual_fragment, R.id.favourite_fragment, R.id.cinemas_fragment,R.id.history_page_fragment, R.id.history_page_fragment
+                R.id.actual_fragment,
+                R.id.favourite_fragment,
+                R.id.cinemas_fragment,
+                R.id.history_page_fragment,
+                R.id.history_page_fragment
             )
         )
 
-       setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
